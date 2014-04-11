b11084a24259f62c2da1ddf864e598f254ff9e20
Ben Hughes <ben@railsgarden.com>
2014-04-10 18:55:43 -0700

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MagicHangman::Application.config.secret_key_base = '36a19790a0165d5430542b042844ff445f0a4e4fa535742469c96ec975290e60e3569cbc55f14a280d219fe60a84498bda69de67853f3ab03a0ed7cf4e615eed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MagicHangman::Application.config.session_store :cookie_store, key: '_magic_hangman_session'
